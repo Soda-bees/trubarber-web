be01f0350be18debd1c439777aef963157d72b50
Simation Studios <brian@simationstudios.com>
2024-10-08 18:19:16 +0500

    sidebar issue fixed

--- src/screens/Layout/index.tsx
+++ src/screens/Layout/index.tsx
@@ -17,7 +17,7 @@ const Layout = (props: Props) => {
   const location = useLocation();
   const noHeaderPaths = ['/signin', '/signup', '/create-user-profile', '/create-barber-profile'];
   const noSidebarPaths = ['/signin', '/signup', '/create-user-profile', '/create-barber-profile']
-  const shouldShowSidebar =  !noSidebarPaths.includes(location.pathname);
+  const shouldShowSidebar = authToken && !noSidebarPaths.includes(location.pathname);
   const shouldShowHeader = !noHeaderPaths.includes(location.pathname);
   return (
     <div>
